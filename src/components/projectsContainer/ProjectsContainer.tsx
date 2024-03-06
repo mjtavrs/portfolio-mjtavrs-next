7a237073551afe95eb14fa9a4f4d67a81d5b5fd7
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-06 06:36:41 -0300

    refactor: disable the react/jsx-key for the entire projectsContainer component

--- src/components/projectsContainer/ProjectsContainer.tsx
+++ src/components/projectsContainer/ProjectsContainer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import ProjectCard from "../shared/projectCard/ProjectCard";
 
 import { IoLogoJavascript } from "react-icons/io5";
@@ -8,7 +9,6 @@ export default function ProjectsContainer() {
         <div className="flex sm:flex-col md:flex-row items-center">
             <ProjectCard
                 projectCover="https://placehold.co/320x400"
-                // eslint-disable-next-line react/jsx-key
                 technologiesUsed={[<IoLogoJavascript />, <FaSass />, <FaGitAlt />]}
                 projectTitle="Maribe Arquitetura"
                 projectDesc="Maribe é um escritório de arquitetura e urbanismo baseado em Recife, Pernambuco. Seu projeto solicitava uma página de apresentação, uma página para mostrar os projetos feitos pelo escritório, além de dois formulários, um para orçamento, outro para contato. Criar esse projeto do zero, desde o design até o tratamento back-end, me possibilitou um grande aprendizado que carrego desde então."
